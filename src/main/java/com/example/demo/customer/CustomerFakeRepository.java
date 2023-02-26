1b14a75a4ca6f7576ec0cd90d0ffda540e6e4dd7
Macailh <salvadorenriquegb@gmail.com>
2023-02-26 16:20:00 -0600

    Add alternative implementation for customer repo

---
+++ src/main/java/com/example/demo/customer/CustomerFakeRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.customer;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class CustomerFakeRepository implements CustomerRepo{
+    @Override
+    public List<Customer> getCustomers() {
+        return Arrays.asList(
+                new Customer(1L, "James Bond", "hola123"),
+                new Customer(2L, "Jamila Ahomand", "qwerty")
+        );
+    }
+}
