74dca7fef2558702c76a8c4b0b96cf0c83135f0a
Macailh <salvadorenriquegb@gmail.com>
2023-02-26 16:19:26 -0600

    Add controller for customer

--- src/main/java/com/example/demo/customer/CustomerController.java
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -1,13 +1,41 @@
 package com.example.demo.customer;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequestMapping("api/v1/customers")
 @RestController
+@Deprecated
 public class CustomerController {
 
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @GetMapping
-    Customer getCustomer() {
-        return new Customer(1L, "James Bond");
+    List<Customer> getCustomers() {
+        return customerService.getCustomers();
+    }
+
+    @PostMapping
+    void createNewCustomer(@RequestBody Customer customer) {
+        System.out.println("POST REQUEST... ");
+        System.out.println(customer);
+    }
+    @PutMapping
+    void updateCustomer(@RequestBody Customer customer) {
+        System.out.println("UPDATE REQUEST... ");
+        System.out.println(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    void deleteCustomer(@PathVariable("customerId") Long id) {
+        System.out.println("DELETE REQUEST... ");
+        System.out.println(id);
     }
 }
