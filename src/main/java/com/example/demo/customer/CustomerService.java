276efdfb853f380c020e66e7c533a8dab13b5733
Macailh <salvadorenriquegb@gmail.com>
2023-02-26 16:18:42 -0600

    Add service layer for customer

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -1,4 +1,29 @@
 package com.example.demo.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+
+    private final CustomerRepo customerRepo;
+
+    @Autowired
+    public CustomerService(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+    List<Customer> getCustomers() {
+        return customerRepo.getCustomers();
+    }
+
+    Customer getCustomer(@PathVariable("customerId") Long id) {
+        return customerRepo.getCustomers()
+                .stream()
+                .filter(customer -> customer.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Customer not found"));
+    }
 }
