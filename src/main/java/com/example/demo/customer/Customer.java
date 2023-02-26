23a379d7caa4a14477554b555162a877c9ab3553
Macailh <salvadorenriquegb@gmail.com>
2023-02-26 16:17:15 -0600

    Config json for customer

--- src/main/java/com/example/demo/customer/Customer.java
+++ src/main/java/com/example/demo/customer/Customer.java
@@ -1,14 +1,22 @@
 package com.example.demo.customer;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Customer {
     private final Long id;
     private final String name;
 
-    public Customer(Long id, String name) {
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private final String password;
+
+    public Customer(Long id, String name, String password) {
         this.id = id;
         this.name = name;
+        this.password = password;
     }
 
+    @JsonProperty("customerId")
     public Long getId() {
         return id;
     }
@@ -17,11 +25,17 @@ public class Customer {
         return name;
     }
 
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
     @Override
     public String toString() {
         return "Customer{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 }
