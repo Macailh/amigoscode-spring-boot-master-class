77b34998f133fab718e8a03875bc9d7fd878c93d
Macailh <salvadorenriquegb@gmail.com>
2023-02-26 16:18:18 -0600

    Add interface and implements for customer

--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -1,4 +1,13 @@
 package com.example.demo.customer;
 
-public class CustomerRepository {
+import java.util.List;
+
+public class CustomerRepository implements CustomerRepo{
+    @Override
+    public List<Customer> getCustomers() {
+        // TODO connect to real db
+        return List.of(
+                new Customer(1L, "TODO, Implement real db", "password")
+        );
+    }
 }
